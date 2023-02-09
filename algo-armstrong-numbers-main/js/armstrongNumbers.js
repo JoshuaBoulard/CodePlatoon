7baadb97d2dfc94f6c7f75ab01673a30973b7c1f
JoshuaBoulardJr <thegrey405@gmail.com>
2023-02-09 12:18:11 -0800

    answered armstrong

--- algo-armstrong-numbers-main/js/armstrongNumbers.js
+++ algo-armstrong-numbers-main/js/armstrongNumbers.js
@@ -1,5 +1,22 @@
-// How can you make this more scalable and reusable later?
+function armstrong(arr) {
+    let answer = [];
+    let list = []
+    for (let i = 0; i < arr.length; i++) {
+        let count = arr[i].toString().length;
+        let splitNum = arr[i].toString().split('');
+        let nums = 0;
 
-exports.findArmstrongNumbers = function() {
-
-};
+        for (let j = 0; j < splitNum.length; j++) {
+            let powers = Number(splitNum[j])**Number(count)
+            nums += powers
+    
+        }
+        list.push(nums)
+        if (arr[i] == nums) {
+            answer.push(arr[i])
+            console.log(answer)
+        }
+        
+    }
+}
+console.log(armstrong([371,40,50]))

204d4d1139a02866a5112c1e2fd285f110fab152
JoshuaBoulardJr <thegrey405@gmail.com>
2023-02-10 07:22:55 -0800

    Added more assignments of D5

--- algo-character-count-main/python/char_count.py
+++ algo-character-count-main/python/char_count.py
@@ -1,2 +1,9 @@
 def char_count(str):
-  # Your code here
+  charDict = {}
+  for letter in str: 
+    if letter in charDict:
+      charDict[letter] += 1
+    else: charDict[letter] = 1
+  print(charDict)
+
+char_count('apple')

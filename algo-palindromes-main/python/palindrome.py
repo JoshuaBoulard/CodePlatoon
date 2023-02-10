52dd00416aae087ce8bb3c1e970e06a537985fb6
JoshuaBoulardJr <thegrey405@gmail.com>
2023-02-10 08:03:35 -0800

    Solved palindrome & partially solved char count

--- algo-palindromes-main/python/palindrome.py
+++ algo-palindromes-main/python/palindrome.py
@@ -1,2 +1,16 @@
-def palindrome(word):
-    # Write code here
+def palindrome(string):
+    string = str(string)
+    string = string.lower()
+    new_string = ''
+    for char in string:
+        if char.isalnum():
+            new_string += char
+        
+    reverse_string = new_string[::-1]
+   
+    if reverse_string == new_string:
+        return True
+    else:
+        return False
+
+print(palindrome('A man, a plan, a canal -- Panama'))

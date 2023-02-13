70554303e59f87891c1be06c6ca4445ba257e772
JoshuaBoulardJr <thegrey405@gmail.com>
2023-02-13 10:00:08 -0800

    Solved the problem

--- oop-app-users-i-master/User.py
+++ oop-app-users-i-master/User.py
@@ -1,1 +1,16 @@
-# your User class goes here
+class User:
+
+    def __init__(self, first_name, last_name, username, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email
+
+    def __str__(self):
+        return f"Name: {self.first_name + ' ' + self.last_name} Username: {self.username} Email: {self.email}"
+
+mike = User("Mike", "Williams", "DirtyMike", "mikewilliams@email.com")
+will = User("Will", "Johnson", "WillyPete", "willjohnson@email.com")
+john = User('John', "Hancock", "JohnnyBravo", "johnhancock@email.com")
+
+print(mike)
--- oop-guessing-game-master/guessing_game.py
+++ oop-guessing-game-master/guessing_game.py
@@ -1,1 +1,20 @@
 class GuessingGame():
+    answer = 17
+    user_guess = None
+    guessing_game_solved = False
+
+    while guessing_game_solved == False:
+        if user_guess != None:
+            if user_guess > answer:
+                print("High")
+            elif user_guess < answer:
+                print("Low")
+            else:
+                guessing_game_solved = True
+                print("Correct")
+                break
+
+        user_guess = int(input("Enter your guess here => "))
+    
+
+GuessingGame()

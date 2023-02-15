58a77ebe1e2e415561c90a180389a57678a4b933
JoshuaBoulardJr <thegrey405@gmail.com>
2023-02-14 18:14:55 -0800

    Solved one problem, got stuck on contact-list

--- oop-app-users-i-master/User.py
+++ oop-app-users-i-master/User.py
@@ -6,11 +6,34 @@ class User:
         self.username = username
         self.email = email
 
+        if not hasattr(User, 'posts'):
+            User.posts = []
+
+
+
     def __str__(self):
         return f"Name: {self.first_name + ' ' + self.last_name} Username: {self.username} Email: {self.email}"
+
+    @staticmethod
+    def make_post(self):
+        post = input("Make a post:\n")
+        User.posts.append(f'{self.username}: {post}')
+        return f"{self.username} posted:\n{post}"
 
 mike = User("Mike", "Williams", "DirtyMike", "mikewilliams@email.com")
 will = User("Will", "Johnson", "WillyPete", "willjohnson@email.com")
 john = User('John', "Hancock", "JohnnyBravo", "johnhancock@email.com")
 
-print(mike)
+mike_posts = User.posts
+will_posts = User.posts
+john_posts = User.posts
+
+print(User.make_post(mike))
+print(User.make_post(will))
+print(User.make_post(john))
+
+print(mike_posts)
+print(will_posts)
+print(john_posts)
+
+
